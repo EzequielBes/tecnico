9b1ebe6312fe1d2e1717f5e7cc20f0eded3fe5a3
Ezequiel <113632597+EzequielBes@users.noreply.github.com>
2024-10-07 14:32:26 -0300

    Update updateAccount.ts

--- backend/src/application/usecases/account/updateAccount.ts
+++ backend/src/application/usecases/account/updateAccount.ts
@@ -1,5 +1,6 @@
 import { inject } from "../../../di/registry";
 import { AccountRepository } from "../../../infra/repository/databaseRepository";
+import { AppError } from "../../../utils/errormap";
 
 export class UpdateAccountUseCase {
   @inject("accountRepository")
@@ -7,13 +8,18 @@ export class UpdateAccountUseCase {
 
   constructor () {}
 
-  async execute(input: accountUpdated):Promise<string> {
-    if(!input.name || !input.email) throw new Error("Name or Email not valid");
+  async execute(input: accountUpdated):Promise<string | void> {
+    try {
+    if(!input.name || !input.email) throw new AppError("Name or Email not valid", 400);
     const account = await this.accountRepository.getAccountByEmail(input.email);
-    if (!account) throw new Error("Account not exists");
+    if (!account) throw new AppError("Account not exists", 404);
     const updatedAccount = account.update(input.email, input.name)
     await this.accountRepository.updateAccount(updatedAccount);
     return "Account Updated"
+  }catch (error:any) {
+      if(error.statusCode) throw new AppError(error.message, error.statusCode)
+      if(!error.statusCode)  throw new AppError(error.message, 402)
+  }
   }
 }
 
