0d482d2e8cc18a0bef635756e8cc5775b041e63c
Ezequiel <113632597+EzequielBes@users.noreply.github.com>
2024-10-07 14:33:49 -0300

    Update updateDocument.ts

--- backend/src/application/usecases/documents/updateDocument.ts
+++ backend/src/application/usecases/documents/updateDocument.ts
@@ -2,6 +2,7 @@ import { inject } from "../../../di/registry";
 import { Document } from "../../../domain/entity/Document";
 import { InputDocument } from "../../../infra/interfaces/Document";
 import { DocumentDatabaseRepository } from "../../../infra/repository/documentDatabase";
+import { AppError } from "../../../utils/errormap";
 
 export class UpdateDocument {
   @inject("documentRepository")
@@ -9,15 +10,20 @@ export class UpdateDocument {
 
   constructor() { }
   async execute(input : document) {
-    if (!input.document_id) throw new Error("Invalid format");
-    const document = await this.documentRepository.getDocumentById(input.document_id)
-    if (!document) throw new Error("Document not finded");
-    const updatedDocument = document.update(input.document_name, input.status)
-    await this.documentRepository.updateDocument(updatedDocument)
-    return "Document Updatted"
+    try {
+
+      if (!input.document_id) throw new AppError("Invalid format", 400);
+      const document = await this.documentRepository.getDocumentById(input.document_id)
+      if (!document) throw new AppError("Document not finded", 404);
+      const updatedDocument = document.update(input.document_name, input.status)
+      await this.documentRepository.updateDocument(updatedDocument)
+      return "Document Updatted"
+    }catch (error:any) {
+        if(error.statusCode) throw new AppError(error.message, error.statusCode)
+        if(!error.statusCode)  throw new AppError(error.message, 402)
+    }
   }
 }
-
 
 type document = {
   document_id: string,
