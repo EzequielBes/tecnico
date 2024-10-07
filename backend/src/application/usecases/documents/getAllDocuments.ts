9efc582174eb8bfb0495e4195a2d0f6ae4e02cab
Ezequiel <113632597+EzequielBes@users.noreply.github.com>
2024-10-07 14:33:18 -0300

    Update getAllDocuments.ts

--- backend/src/application/usecases/documents/getAllDocuments.ts
+++ backend/src/application/usecases/documents/getAllDocuments.ts
@@ -1,5 +1,6 @@
 import { inject } from "../../../di/registry";
 import { DocumentDatabaseRepository } from "../../../infra/repository/documentDatabase";
+import { AppError } from "../../../utils/errormap";
 
 export class GetAllDocuments {
   @inject("documentRepository")
@@ -7,8 +8,14 @@ export class GetAllDocuments {
 
   constructor() { }
   async execute(input: {userId: string}) {
-    if (!input.userId) throw new Error("Invalid format");
-    const getDocuments = await this.documentRepository.getDocumentByUser(input.userId)
-    return getDocuments
-  }
+    try {
+
+      if (!input.userId) throw new AppError("Invalid format", 400);
+      const getDocuments = await this.documentRepository.getDocumentByUser(input.userId)
+      return getDocuments
+    }catch (error:any) {
+        if(error.statusCode) throw new AppError(error.message, error.statusCode)
+        if(!error.statusCode)  throw new AppError(error.message, 402)
+    }
+    }
 }
