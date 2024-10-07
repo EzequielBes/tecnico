365d8a0b2dd5e0201f32d922c72b1050b6f191c0
Ezequiel <113632597+EzequielBes@users.noreply.github.com>
2024-10-07 14:31:26 -0300

    Update createAccount.ts

--- backend/src/application/usecases/account/createAccount.ts
+++ backend/src/application/usecases/account/createAccount.ts
@@ -1,21 +1,28 @@
 import { inject } from "../../../di/registry";
 import { Account } from "../../../domain/entity/Account";
 import { AccountRepository } from "../../../infra/repository/databaseRepository";
+import { AppError } from "../../../utils/errormap";
 
 export class CreateAccountUseCase {
   @inject("accountRepository")
   readonly accountRepository! : AccountRepository
   constructor () {}
 
-  async execute(input: accountDTO):Promise<string> {
-    if(!input.name || !input.email) throw new Error("Name or Email not valids");
-    
-    const accountExists = await this.accountRepository.getAccountByEmail(input.email);
-    if (accountExists) throw new Error("Account already exists");
-    const account = Account.create(input.email, input.name)
-    await this.accountRepository.saveAccount(account);
-    return "Account created"
-  }
+  async execute(input: accountDTO):Promise<string | void> {
+    try {
+      if(!input.name || !input.email) throw new AppError("Name or Email not valids", 400);
+      const accountExists = await this.accountRepository.getAccountByEmail(input.email);
+      if (accountExists) throw new AppError("Account already exists", 409);
+      const account = Account.create(input.email, input.name)
+      await this.accountRepository.saveAccount(account);
+      return "Account created"
+    }
+    catch(error:any) {
+        if(error.statusCode) throw new AppError(error.message, error.statusCode)
+        if(!error.statusCode)  throw new AppError(error.message, 402)
+       
+    }
+  } 
 }
 
 type accountDTO = {
