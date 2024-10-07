f94d375ee83ae60b76eedb9ce5370240f368481c
Ezequiel <113632597+EzequielBes@users.noreply.github.com>
2024-10-07 14:33:03 -0300

    Update deleteDocument.ts

--- backend/src/application/usecases/documents/deleteDocument.ts
+++ backend/src/application/usecases/documents/deleteDocument.ts
@@ -1,6 +1,7 @@
 import { inject } from "../../../di/registry";
 
 import { DocumentDatabaseRepository } from "../../../infra/repository/documentDatabase";
+import { AppError } from "../../../utils/errormap";
 import { validateToken } from "../authentication/jwtValidate";
 
 export class DeleteDocument {
@@ -9,11 +10,17 @@ export class DeleteDocument {
 
   constructor() { }
   async execute(params:{document_id: string}, authHeader:string) {
-  const token = authHeader.split(" ")[1];
-    if (!token) throw new Error("Token missing");
-    if(!validateToken(token)) throw new Error("Invalid token")
-    if (!params.document_id ) throw new Error("Invalid format");
-    await this.documentRepository.deleteDocument(params.document_id)
-    return "Document Deleted"
+    try {
+
+      const token = authHeader.split(" ")[1];
+      if (!token) throw new Error("Token missing");
+      if(!validateToken(token)) throw new AppError("Invalid token", 401)
+        if (!params.document_id ) throw new AppError("Invalid format", 400);
+      await this.documentRepository.deleteDocument(params.document_id)
+      return "Document Deleted"
+    } catch (error:any) {
+        if(error.statusCode) throw new AppError(error.message, error.statusCode)
+        if(!error.statusCode)  throw new AppError(error.message, 402)
+    }
   }
 }
