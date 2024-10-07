51bcf42977a2699a588dc48050790dec55371195
Ezequiel <113632597+EzequielBes@users.noreply.github.com>
2024-10-07 14:32:51 -0300

    Update createDocument.ts

--- backend/src/application/usecases/documents/createDocument.ts
+++ backend/src/application/usecases/documents/createDocument.ts
@@ -1,6 +1,7 @@
 import { inject } from "../../../di/registry";
 import { Document } from "../../../domain/entity/Document";
 import { DocumentDatabaseRepository } from "../../../infra/repository/documentDatabase";
+import { AppError } from "../../../utils/errormap";
 import { validateToken } from "../authentication/jwtValidate";
 
 export class CreateDocument {
@@ -8,14 +9,20 @@ export class CreateDocument {
   readonly documentRepository!: DocumentDatabaseRepository
 
   constructor() { }
-  async execute(input : document, authHeader: string):Promise<string> {
-    const token = authHeader.split(" ")[1];
-    if (!token) throw new Error("Token missing");
-    if(!validateToken(token)) throw new Error("Invalid token")
-    if (!input.document_name || !input.status || !input.account_id) throw new Error("Invalid format");
-    const document = Document.create(input.document_name, input.status, input.account_id);
-    await this.documentRepository.saveDocument(document)
-    return "Document Created"
+  async execute(input : document, authHeader: string):Promise<string | void> {
+    try{
+
+      const token = authHeader.split(" ")[1];
+      if (!token) throw new AppError("Token missing", 401);
+      if(!validateToken(token)) throw new Error("Invalid token")
+        if (!input.document_name || !input.status || !input.account_id) throw new AppError("Invalid format",400);
+      const document = Document.create(input.document_name, input.status, input.account_id);
+      await this.documentRepository.saveDocument(document)
+      return "Document Created"
+    } catch (error:any) {
+        if(error.statusCode) throw new AppError(error.message, error.statusCode)
+        if(!error.statusCode)  throw new AppError(error.message, 402)
+    }
   }
 }
 
